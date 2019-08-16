4145ab1a717c59b6f7c8836f54a6c4d965654e81
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-08-16 23:59:55 +0530

    Add files via upload

---
+++ D.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+class D{
+public static void main(String[] args){
+int t, n;
+BufferedReader inp = new BufferedReader (new InputStreamReader(System.in));
+
+t=Integer.parseInt(inp.readLine());
+for(int k=0;k<t;k++){
+n=Integer.parseInt(inp.readLine());
+int arr[]= new int[n];
+String s =inp.readLine();
+String[] s1=s.split(" ");
+for(int i=0;i<n;i++){
+arr[i]=Integer.parseInt(s1[i]);
+}
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+
+if(arr[i]>arr[j])
+{if(j==n-1){
+System.out.print(arr[i]+" ");
+}
+}
+}
+}
+}
+}
+}
+
+
