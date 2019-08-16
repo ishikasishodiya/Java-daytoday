52b2d092d6bd4d4a67730dc3f2e8cccc506796f4
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-08-16 23:55:18 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class E {
+
+public static void main (String[] args)
+	
+ {
+
+ int t,n,m;
+
+Scanner sc = new Scanner(System.in);
+
+t=sc.nextInt();
+
+
+for(int k=0;k<t;k++){
+
+n=sc.nextInt();
+
+int arr[]= new int[n];
+
+for(int i=0;i<n;i++)
+{
+arr[i]=sc.nextInt();
+
+}
+
+for(int i=n-1;i>=0;i--){
+
+    System.out.print(arr[i]+" ");
+
+}
+System.out.print("\n");
+
+
+	 }
+
+}
+}
