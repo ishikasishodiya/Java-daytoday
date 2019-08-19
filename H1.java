110c6cee80b2afb4cf12d0a370f455db27139ead
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-08-19 23:18:57 +0530

    Add files via upload

---
+++ H1.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.io.*;
+class H1{
+public static void main(String[] args) throws IOException{
+int t,n;
+BufferedReader br =new BufferedReader( new InputStreamReader(System.in));
+t= Integer.parseInt(br.readLine());
+for(int k=0;k<t;k++){
+int temp;
+n= Integer.parseInt(br.readLine());
+int arr[] = new int[n];
+String s = br.readLine();
+String[] str = s.split(" ");
+for(int i=0;i<n;i++){
+arr[i]= Integer.parseInt(str[i]);
+}
+ int min = arr[0];
+	       int index=0;
+
+	       for(int i = 0; i < n; i++)
+	       {
+	            if(min > arr[i])
+	            {
+	                min = arr[i];
+	                index=i;
+	            }
+	        }
+
+if(n%2!=0){
+temp=arr[n/2];
+arr[n/2]=arr[n-1];
+arr[n-1]=temp;
+/*for(int i=0;i<n;i++){
+System.out.print(arr[i]);
+} 
+ System.out.print("\n"); */
+for(int i=0;i<n/2;i++){
+for(int j=i+1;j<n/2;j++){
+if(arr[i]<arr[j]){
+temp = arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+}
+for(int i=0;i<n/2;i++){
+System.out.print(arr[i]+" ");
+}
+
+for(int i=(n/2)+1;i<n;i++){
+for(int j=i+1;j<n;j++){
+if(arr[i]<arr[j]){
+temp = arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+}
+for(int i=(n/2)+1;i<0;i++){
+System.out.print(arr[i]+" ");
+}
+}
+else if(n%2==0){
+temp=arr[(n-1)/2];
+arr[(n-1)/2]=arr[n-1];
+arr[n-1]=temp;
+/*for(int i=0;i<n;i++){
+System.out.print(arr[i]);
+} */
+ System.out.print("\n");
+for(int i=0;i<(n-1)/2;i++){
+for(int j=i+1;j<(n-1)/2;j++){
+if(arr[i]<arr[j]){
+temp = arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+}
+for(int i=0;i<(n-1)/2;i++){
+System.out.print(arr[i]+" ");
+}
+
+for(int i=((n-1)/2)+1;i<n;i++){
+for(int j=i+1;j<n;j++){
+if(arr[i]<arr[j]){
+temp = arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+}
+for(int i=((n-1)/2)+1;i<0;i++){
+System.out.print(arr[i]+" ");
+} 
+}
+
+
+
+
+}
+}
+}
