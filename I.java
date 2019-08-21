8ab58950caa57a008865fc2fbc355003cfec1080
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-08-21 20:36:42 +0530

    Add files via upload

---
+++ I.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+class I{
+public static void main(String[] args) throws IOException{
+int t,n,d;
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+t= Integer.parseInt(br.readLine());
+for(int k=0;k<t;k++){
+String s = br.readLine();
+String[] str=s.split(" ");
+n=Integer.parseInt(str[0]);
+
+d=Integer.parseInt(str[1]); 
+int arr[]= new int[n];
+String s2 = br.readLine();
+String[] s12 = s2.split(" ");
+for(int i=0;i<n;i++)
+{
+    arr[i]=Integer.parseInt(s12[i]);
+ 
+}
+
+ 
+int a=0;
+int cdis=Math.abs(arr[0]-d);
+for(int i=0;i<n;i++){
+int distance=Math.abs(arr[i]-d);           
+if(distance<=cdis)
+{
+a=arr[i];
+cdis= distance;
+}
+
+
+}
+
+System.out.println(a);
+
+
+
+
+
+}
+}
+}
