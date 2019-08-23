d487d73855c4db4a489919b896c05e7f7edd5948
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-08-23 23:52:56 +0530

    Add files via upload

---
+++ K.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+class K{
+public static void main(String[] args) throws IOException{
+int t,n,m,data[],index=0;
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+t = Integer.parseInt(br.readLine);
+for(int k=0;k<n;k++){
+n= Integer.parseInt(br.readLine);
+int arr[]= new arr[n];
+String s =br.readLine();
+String[] s1 = s.split(" ");
+for(int i=0;i<n;i++){
+arr[i]= Integer.parseInt(s1[n]);
+}
+m= Integer.parseInt(br.readLine);
+ int r = m;
+int i=0;
+int data[] = new int[r];
+if (index == r) { 
+            for (int j = 0; j < r; j++) 
+                System.out.print(data[j] + " "); 
+            System.out.println(""); 
+            return; 
+        } 
+
+
+
+
+
+}
+}
+}
