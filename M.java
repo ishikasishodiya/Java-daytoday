3bd7a99a786e2cbcd3fb956fb0c4e98c70cc4e22
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-08-25 01:10:54 +0530

    Add files via upload

---
+++ M.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class M{
+public static void main(String[] args) throws IOException{
+int t;
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+t = Integer.parseInt(br.readLine());
+for(int k=0;k<t;k++){
+String s1 = br.readLine();
+String s2 = br.readLine();
+StringBuffer sb1 = new StringBuffer(s1.replace(" "," "));  //deleteCharAt is used with StringBuffer only.
+StringBuffer sb2 = new StringBuffer(s2.replace(" "," "));
+int a=0;
+for(int i=0;i<sb1.length();i++){
+for(int j=0;j<sb2.length();j++){
+if(sb1.charAt(i)== sb2.charAt(j)){
+char x=sb1.charAt(i);
+char y=sb2.charAt(i);
+int x1=sb1.indexOf(x,i);
+int y1=sb2.indexOf(y,j);
+sb1.deleteCharAt(i);
+sb2.deleteCharAt(j);
+sb1.deleteCharAt(x);
+sb2.deleteCharAt(y);
+
+}
+}
+}
+
+StringBuffer b = new StringBuffer(100);
+b.append(sb1).append(sb2);
+System.out.println(b.toString());
+}
+}
+}
