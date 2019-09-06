a442388a7ba207f76b5ab31c073e188c69b3409f
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-09-07 00:07:32 +0530

    Update P1.java

--- P1.java
+++ P1.java
@@ -18,9 +18,10 @@ for(int i=0;i<n;i++)
     arr[i]=Integer.parseInt(s12[i]);
 }
 int max; int a=0; int b=0;
-max=arr[0];
-for(int i=0;i<n;i++){
-for(int j=i;j<d+i && j<n-d+1;j++){
+
+for(int i=0;i<n-d+1;i++){
+ max= arr[i];
+for(int j=i;j<d+i && j<n;j++){
 if(arr[j]>max){
 max = arr[j];
 
