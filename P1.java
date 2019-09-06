e6d6838ce757448dbe8a9f2360a40c5f994ae587
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-09-06 23:57:12 +0530

    Update P1.java

--- P1.java
+++ P1.java
@@ -1,4 +1,4 @@
-import java.util.*;
+ import java.util.*;
 import java.io.*;
 class P1{
 public static void main(String[] args) throws IOException{
@@ -18,8 +18,8 @@ for(int i=0;i<n;i++)
     arr[i]=Integer.parseInt(s12[i]);
 }
 int max; int a=0; int b=0;
+max=arr[0];
 for(int i=0;i<n;i++){
-max=arr[i];
 for(int j=i;j<d+i && j<n;j++){
 if(arr[j]>max){
 max = arr[j];
@@ -28,13 +28,7 @@ max = arr[j];
 
 }
 System.out.print(max+" ");
-for(int j=i;j<d+i+1&& j<n;j++){
-if(arr[j]<max){
-max=arr[i]
-//b= arr[i];
-}
-}
-System.out.print(max+" ");
+
 }
 System.out.print("\n");
 }
