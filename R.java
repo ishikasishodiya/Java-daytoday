5ebc86461e49765c93d063bafb6044aa15a8b243
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-09-08 23:54:55 +0530

    Add files via upload

---
+++ R.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+class R{
+public static void main(String[] args){
+int t,n;
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+t = Integer.parseInt(br.readLine());
+for(int k=0;k<t;k++){
+n=Integer.parseInt(br.readLine());
+int arr[] = new int[n];
+String s = br.readLine();
+String[] s1 = s.split(" ");
+for(int i=0;i<n;i++){
+arr[i]= Integer.parseInt(s1[i]);
+}
+if(arr.length=1){
+System.out.print(Arrays.toString(arr[0]));
+}
+else if(arr.length>1){
+for(int i=0;i<n;i++){
+
+
+
+}
+
+}
+
+
+}
+}
+}
