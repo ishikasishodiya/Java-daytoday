d2acd6d177798e15afcbea9310068cb827d50b00
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-09-09 01:22:17 +0530

    Update R.java

--- R.java
+++ R.java
@@ -13,19 +13,38 @@ String[] s1 = s.split(" ");
 for(int i=0;i<n;i++){
 arr[i]= Integer.parseInt(s1[i]);
 }
-if(arr.length=1){
-System.out.print(Arrays.toString(arr[0]));
+int a,b=0;
+if(n==1){
+    
+System.out.print("1");
 }
-else if(arr.length>1){
+int sum1=0; int sum2=0; int c=0;
+if(n>1){
 for(int i=0;i<n;i++){
-
-
-
-}
+for(int j=0;j<i;j++){
+sum1= sum1+arr[j];
 
 }
+//System.out.println("sum1 "+ sum1);
+for(int m=i+1;m<n;m++){
+sum2= sum2+arr[m];
+}
+//System.out.println("sum2 "+sum2);
+if(sum1==sum2){
+b++;
+c=i;
+}
+sum1=0; sum2=0;
+}
 
-
+if(b==1){
+System.out.print(c+1);
+}
+if(b!=1){
+System.out.print("-1");
+}
+}
+System.out.print("\n");
 }
 }
 }
