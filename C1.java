d3f23d9e327fcc57004492a381793aa186ac815f
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-09-14 20:28:48 +0530

    Update C1.java

--- C1.java
+++ C1.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.io.*;  // ye imp hai in bufferreader
+import java.io.*;  // ye imp hai in bufferreader...
 class C1
  {
 public static void main (String[] args) throws Exception
