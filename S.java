b66b21f2a6f907bcd029b5f7d772d79e0b883ef4
Ishika Singh <35527764+ishikasishodiya@users.noreply.github.com>
2019-09-16 22:49:47 +0530

    Update S.java

--- S.java
+++ S.java
@@ -1,10 +1,10 @@
 import java.util.*;
 import java.io.*;
 class S{
-public static void main(String[] args) throws IOExcetion{
+public static void main(String[] args) throws IOException{
 int t,n,d;
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-t= Integer.parseInt(br.readline());
+t= Integer.parseInt(br.readLine());
 for(int k=0;k<t;k++){
 String a = br.readLine();
 String[] b = a.split(" ");
@@ -16,16 +16,31 @@ int arr[] = new int[n];
 for(int i=0;i<n;i++){
 arr[i]= Integer.parseInt(b1[i]);
 }
+int start;
+int header=0;
+int wsum=0;
+int flag=0 ;// to check whether we have found the substring with given sum or not
 
 for(int i=0;i<n;i++){
-int start= i;
-sum=sum+arr[i];
-if(sum>d)
+wsum= wsum +arr[i]; // this statement should be above while, test case disapproved
+while(wsum>d){  // if se saare test case paas nhi hue
+wsum= wsum - arr[header];
+header++;
+}
+if(wsum ==d){
+flag=1;
 
+System.out.print(header+1 +" ");
+System.out.print(i+1 +" ");
+break;
+}
 
-
-
-
+}
+if(flag==0)
+{
+System.out.print("-1");
+}
+System.out.print("\n");
 }
 }
 }
